1db9a1175f35d3a8691d2a4770670c748e75339b
Eroica-cpp <eroicacmcs@gmail.com>
2014-06-16 15:56:52 +0800

    add post, get, request demos

--- forms/welcome.php
+++ forms/welcome.php
@@ -1,8 +1,20 @@
 <html>
 <body>
 
+<br>POST demo: <br>
 Welcome <?php echo $_POST["name"]; ?>. <br>
 You are <?php echo $_POST["age"]; ?> years old. 
+<br>
+
+<br>GET demo: <br>
+Welcome <?php echo $_GET["name"]; ?>. <br>
+You are <?php echo $_GET["age"]; ?> years old. 
+<br>
+
+<br>REQUEST demo: <br>
+Welcome <?php echo $_REQUEST["name"]; ?>.<br />
+You are <?php echo $_REQUEST["age"]; ?> years old!
+<br>
 
 </body>
 </html>
